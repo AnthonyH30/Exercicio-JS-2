71e3d52394ec7ec8389cfd5a9b136307a474ecda
Anthony <anthonyharisson30@gmail.com>
2022-06-13 17:55:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let idade = 19;
+
+if(idade > 18){
+    console.log('Idade é maior que 18');
+}
+
+let condiçaoHumana = true;
+
+if(idade >= 18 && condiçaoHumana){
+    console.log('verificação confirmada')
+}
+
+let aniversario = 'dezembro'
+
+if(aniversario == 'janeiro' || 'dezembro'){
+    console.log('Seu aniversario é em janeiro ou dezembro!')
+}
